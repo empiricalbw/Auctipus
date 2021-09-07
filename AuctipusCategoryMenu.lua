1fc325e889d8a8cb4dad5eed820d4d0464121a06
Empirical <empirical.bw@gmail.com>
2021-09-07 06:35:42 -0700

    add a "Reset Filters" button to AuctipusCategoryMenu

--- AuctipusCategoryMenu.lua
+++ AuctipusCategoryMenu.lua
@@ -1,6 +1,8 @@
 ACategoryMenu = ADropDown:_New()
 ACategoryMenu.__index = ACategoryMenu
 
+local RESET_FILTERS_HEIGHT = 16
+
 function ACategoryMenu:Init(config)
     ADropDown.Init(self, config)
 
@@ -8,6 +10,26 @@ function ACategoryMenu:Init(config)
         item.UnCheck:Show()
         item.disableCount = 0
     end
+
+    self.frame:SetHeight(self.frame:GetHeight() + RESET_FILTERS_HEIGHT)
+    self.items[1]:SetPoint("TOPLEFT", 12, -10 - RESET_FILTERS_HEIGHT)
+    local f = CreateFrame("Button", nil, self.frame,
+                          "AuctipusDropDownItemTemplate")
+    self.resetButton = f
+    f:SetHeight(RESET_FILTERS_HEIGHT)
+    f:SetWidth(config.width * 3)
+    f:SetPoint("TOPLEFT", 12, -8)
+    f.LabelEnabled:SetText("Reset Filters")
+    f.LabelEnabled:SetJustifyH("CENTER")
+    f.LabelEnabled:SetJustifyV("MIDDLE")
+    f.LabelDisabled:SetText("Reset Filters")
+    f.LabelDisabled:SetJustifyH("CENTER")
+    f.LabelDisabled:SetJustifyV("MIDDLE")
+    f.LabelEnabled:Hide()
+    f:Disable()
+    f.Check:Hide()
+    f.UnCheck:Hide()
+    f:SetScript("OnClick", function() self:ClearSelection() end)
 
     self.selection = ASet:New()
 end
@@ -21,6 +43,16 @@ function ACategoryMenu:OnItemClick(index)
         self.selection:Insert(index)
     else
         self.selection:Remove(index)
+    end
+
+    if self.selection:Empty() then
+        self.resetButton.LabelEnabled:Hide()
+        self.resetButton.LabelDisabled:Show()
+        self.resetButton:Disable()
+    else
+        self.resetButton.LabelEnabled:Show()
+        self.resetButton.LabelDisabled:Hide()
+        self.resetButton:Enable()
     end
 
     ADropDown.OnItemClick(self, index)
